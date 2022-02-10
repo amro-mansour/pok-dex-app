1b990caea8a7f9e4b9fffcb7f04af27493ebad90
amro-mansour <amro8000@gmail.com>
2022-02-10 20:31:08 +0100

    creating the variable pokemonList, with an array
    the Array contains various objects, each with 3 keys describing Pokémons characteristics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-alert('Hello World');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Psyduck', height: 0.8, types: 'water'},
+  {name: 'Gastly', height: 1.3, types: ['ghost','poison']},
+  {name: 'Charizard', height: 1.7, types: ['fire','flying']},
+  {name: 'Mewtwo', height: 2, types: 'psychic'}
+]
