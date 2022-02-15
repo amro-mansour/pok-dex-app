00328b79baa923524aa08dc4f07e1e2fc059c68e
amro-mansour <amro8000@gmail.com>
2022-02-15 19:06:36 +0100

    Loop added to display Pokémons names and their heights on the website’s DOM
    The loop also contains a Conditional to display a special text next to the tallest Pokémon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+// This is an Array containing different objects. These objects contain a list Pokemons with their specific features
 let pokemonList = [
   {name: 'Psyduck', height: 0.8, types: 'water'},
   {name: 'Gastly', height: 1.3, types: ['ghost','poison']},
   {name: 'Charizard', height: 1.7, types: ['fire','flying']},
   {name: 'Mewtwo', height: 2, types: 'psychic'}
 ]
+
+// This is a loop used to display on the browser the Pokemons names and heights, it also displays a special text next to the tallest Pokemon 
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.9) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '- Wow, that’s big!' + '<br>');
+  } else { document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
+  }
+}
