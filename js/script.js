2ebb34132d40c4477020772e2d687aa6e229b05e
amro-mansour <amro8000@gmail.com>
2022-02-22 21:32:55 +0100

    Adding the IIFE function method
    Here the Pokémon list was wrapped inside the IIFE function, inside the function were inserted 2 functions to first allow adding other pokemons to the list and the second  to return the content of the array.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
-// This is an Array containing different objects. These objects contain a list Pokemons with their specific features
-let pokemonList = [
+// This create a funcion with IIFE method in order make the variable contained local and not global
+let pokemonRepository = (function() {
+  let pokemonList = [
   {name: 'Psyduck', height: 0.8, types: 'water'},
-  {name: 'Gastly', height: 1.3, types: ['ghost','poison']},
-  {name: 'Charizard', height: 1.7, types: ['fire','flying']},
+  {name: 'Gastly', height: 1.3, types:
+  ['ghost','poison']},
+  {name: 'Charizard', height: 1.7, types:
+  ['fire','flying']},
   {name: 'Mewtwo', height: 2, types: 'psychic'}
-]
-
-// This is a loop used to display on the browser the Pokemons names and heights, it also displays a special text next to the tallest Pokemon 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.9) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '- Wow, that’s big!' + '<br>');
-  } else { document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
+  ];
+// This function allows to push other elements to the array
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
+
+// This fuction allows to return the content of the array
+  function getAll() {
+    return pokemonList;
+  }
+
+// This will return the two functions defined above, so then they can be called outside of the function
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+// This is a forEach() fucnction used to display the content of the pokemonList array on the browser, this won't work as the array has been wraped around the above IIFE function
+// function logToConsole(pokemon) {
+//   console.log(pokemon);
+// }
+// pokemonList.forEach(logToConsole);
+
+
+// This function is used to add an object to the pokemonList in the above pokemonRepository
+pokemonRepository.add({name: 'Machamp', height: 1.6, types: 'fighting'});
+console.log(pokemonRepository.getAll());
